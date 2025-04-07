36ff643fbefa89f7ad8617678b8a61f961c85cc8
Ilia Munaev <ilyamunaev@gmail.com>
2025-04-07 09:58:24 +0300

    Update: ex00

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -1,32 +1,34 @@
-// megaphone.cpp
-
 #include <iostream>
 #include <cctype>
 
+class Megaphone {
+    public:
+        void yell(int argc, char **argv) const;
+    private:
+        void print_uppercase(const char *str) const;
+};
 
-void print_word (char *word) {
-
-	for (int j = 0; word[j]; j++)
-	{
-		std::cout << (char)std::toupper(word[j]);
-	}
+void Megaphone::print_uppercase(const char *str) const {
+    for (size_t i = 0; str[i]; i++)
+        std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(str[i])));
 }
 
-int main(int argc, char **argv)
-{
-    if (argc == 1)
-    {
+void Megaphone::yell(int argc, char **argv) const {
+    if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-        return 0;
-    }
+        return;
 
-    for (int i = 1; i < argc; i++)
-    {
-		print_word(argv[i]);
+    } for (int i = 1; i < argc; i++) {
+        print_uppercase(argv[i]);
         if (i < argc - 1)
             std::cout << " ";
     }
     std::cout << std::endl;
+}
 
+int main(int argc, char **argv) {
+    Megaphone megaphone;
+
+    megaphone.yell(argc, argv);
     return 0;
 }
