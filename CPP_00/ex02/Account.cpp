0a7734301d7e6c2bd2ac1c0ad0d1c74783808cff
Ilia Munaev <ilyamunaev@gmail.com>
2025-05-14 22:11:45 +0300

    Add: Ex02

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -1,46 +1,97 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Account.cpp                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: Ilia Munaev <ilyamunaev@gmail.com>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/14 11:28:00 by Ilia Munaev       #+#    #+#             */
-/*   Updated: 2025/05/14 12:08:39 by Ilia Munaev      ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
 
-#include "Account.hpp";
+// static member definitions
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
+// constructor
+Account::Account(int initial_deposit)
+    : _accountIndex(_nbAccounts),
+      _amount(initial_deposit),
+      _nbDeposits(0),
+      _nbWithdrawals(0) {
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
 
-void Account::displayStatus(void) const {
-
-	_displayTimestamp();
-
-	// ammount
-	//created
-
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";amount:" << _amount
+              << ";created" << std::endl;
 }
 
-int Account::getNbAccounts(void) {
+// destructor
+Account::~Account() {
+    _nbAccounts--;
+    _totalAmount -= _amount;
 
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";amount:" << _amount
+              << ";closed" << std::endl;
 }
 
-int Account::getTotalAmount(void) {
+// static getters
+int Account::getNbAccounts(void) { return _nbAccounts; }
+int Account::getTotalAmount(void) { return _totalAmount; }
+int Account::getNbDeposits(void) { return _totalNbDeposits; }
+int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
 
-}
-
-int Account::getNbDeposits(void) {
-
-}
-
-int Account::getNbWithdrawals(void) {
-
-}
-
+// static display
 void Account::displayAccountsInfos(void) {
-
-
-
+    _displayTimestamp();
+    std::cout << "accounts:" << getNbAccounts()
+              << ";total:" << getTotalAmount()
+              << ";deposits:" << getNbDeposits()
+              << ";withdrawals:" << getNbWithdrawals()
+              << std::endl;
 }
 
+// display single account status
+void Account::displayStatus(void) const {
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";amount:" << _amount
+              << ";deposits:" << _nbDeposits
+              << ";withdrawals:" << _nbWithdrawals
+              << std::endl;
+}
+
+// check amount
+int Account::checkAmount(void) const {
+    return _amount;
+}
+
+// timestamp formatter
+void Account::_displayTimestamp(void) {
+    std::time_t now = std::time(0);
+    std::tm* local = std::localtime(&now);
+
+    char yyyymmdd[9];
+    std::strftime(yyyymmdd, sizeof(yyyymmdd), "%Y%m%d", local);
+
+    char hhmmss[7];
+    std::strftime(hhmmss, sizeof(hhmmss), "%H%M%S", local);
+
+    std::cout << "[" << yyyymmdd << "_" << hhmmss << "] ";
+}
+
+void	Account::makeDeposit( int deposit ) {
+    _amount += deposit;
+    _nbDeposits += 1;
+
+}
+bool	Account::makeWithdrawal( int withdrawal ) {
+    int saldo = _amount - withdrawal;
+
+    if (saldo >= 0) {
+        _amount -= withdrawal;
+        _nbWithdrawals += 1;
+        return true;
+    }
+
+    return false;
+}
