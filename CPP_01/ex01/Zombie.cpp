db64219a6ad83924955624df4e7faea878dfc26c
Ilia Munaev <ilyamunaev@gmail.com>
2025-07-31 13:11:23 +0300

    Initial commit

---
+++ CPP_01/ex01/Zombie.cpp
@@ -0,0 +1,30 @@
+#include "Zombie.hpp"
+
+// Constructor: using initializer list
+Zombie::Zombie(std::string name) : _name(name) {}
+
+// Destructor
+Zombie::~Zombie(void) {
+  std::cout << _name << ": is destroied\n";
+}
+
+// Create a new Zombie class
+Zombie* newZombie(std::string name) {
+  Zombie* z = new Zombie(name);
+
+  return z;
+}
+
+// Class Zombie method
+void Zombie::announce(void) {
+  std::cout << _name << ": BraiiiiiiinnnzzzZ...\n";
+}
+
+// Create Zombie instance, call annonunce method, destroy instance
+void randomChump(std::string name) {
+  Zombie* z = newZombie(name);
+  z->announce();
+
+  delete z;
+  z = nullptr;
+}
