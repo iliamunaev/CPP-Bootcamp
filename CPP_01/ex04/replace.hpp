36b8aa4703ba1030efe51ab37a573f2d4834ec2d
Ilia Munaev <ilyamunaev@gmail.com>
2025-08-01 15:48:56 +0300

    Delete: unused lib

--- CPP_01/ex04/replace.hpp
+++ CPP_01/ex04/replace.hpp
@@ -1,9 +1,8 @@
 #ifndef REPLACE_04_HPP
 #define REPLACE_04_HPP
 
-#include <fstream>   // For file input/output operations
-#include <iostream>  // For console input/output
-#include <string>    // For string manipulation
+#include <fstream>
+#include <iostream>
 
 void findAndReplace(const std::string& filename, const std::string& s1, const std::string& s2);
 
