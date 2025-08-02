89c5bd1043a037f753f53c95e8afc8ed22b209b6
Ilia Munaev <ilyamunaev@gmail.com>
2025-08-02 22:17:30 +0300

    Refactor

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -1,14 +1,3 @@
-// • A string variable initialized to "HI THIS IS BRAIN".
-// • stringPTR: A pointer to the string.
-// • stringREF: A reference to the string.
-// Your program has to print:
-// • The memory address of the string variable.
-// • The memory address held by stringPTR.
-// • The memory address held by stringREF.
-// And then:
-// • The value of the string variable.
-// • The value pointed to by stringPTR.
-// • The value pointed to by stringREF.
 
 #include <iostream>
 
