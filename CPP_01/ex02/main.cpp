4d24c407190df66014c25324bfd13ecad31c4cbb
Ilia Munaev <ilyamunaev@gmail.com>
2025-08-05 16:15:04 +0300

    Tests: add more tests

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -1,19 +1,22 @@
-
 #include <iostream>
 
 void brain(void) {
-    std::string s = "HI THIS IS BRAIN\n";
+  std::string s = "HI THIS IS BRAIN";
 
-    std::string* stringPTR = &s;
-    std::string& stringREF = s;
+  std::string* stringPTR = &s;
+  std::string& stringREF = s;
 
-    std::cout  << &s << ": memory address of the string variable" << std::endl;
-    std::cout  << stringPTR << ": memory memory address held by stringPTR" << std::endl;
-    std::cout  << &stringREF << ": memory memory address held by stringREF" << std::endl;
+  std::cout << &s << ": memory address of the string variable" << std::endl;
+  std::cout << stringPTR << ": memory address held by stringPTR" << std::endl;
+  std::cout << &stringREF << ": memory address held by stringREF" << std::endl;
+
+  std::cout << s << ": value pointed to by variable" << std::endl;
+  std::cout << *stringPTR << ": value pointed to by stringPTR" << std::endl;
+  std::cout << stringREF << ": value referenced by stringREF" << std::endl;
 }
 
 int main(void) {
-    brain();
+  brain();
 
-    return 0;
+  return 0;
 }
