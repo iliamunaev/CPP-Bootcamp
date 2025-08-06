e141d592bf0f946470b2a47b58fa17c410809bf9
Ilia Munaev <ilyamunaev@gmail.com>
2025-08-06 19:15:56 +0300

    Refactor: code style

--- CPP_01/ex05/main.cpp
+++ CPP_01/ex05/main.cpp
@@ -1,12 +1,10 @@
 #include "Harl.hpp"
 
 // ------------------ VALIDATION ------------------ //
-bool isLevelValid(const std::string &level)
-{
+bool isLevelValid(const std::string &level) {
   const static std::string s_levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
-  for (int i = 0; i < NUM_LEVELS; i++)
-  {
+  for (int i = 0; i < NUM_LEVELS; i++) {
     if (level == s_levels[i])
       return true;
   }
@@ -14,25 +12,21 @@ bool isLevelValid(const std::string &level)
   return false;
 }
 
-bool isNumArgumentsValid(const int numArguments)
-{
+bool isNumArgumentsValid(const int numArguments) {
   return numArguments == 2;
 }
 
-bool isInputValid(int argc, char **argv)
-{
+bool isInputValid(int argc, char **argv) {
 
   const int numArguments = argc;
-  if (!isNumArgumentsValid(numArguments))
-  {
-    std::cout << "Validation error: Usage ./Harl <level>" << std::endl;
+  if (!isNumArgumentsValid(numArguments)) {
+    std::cerr << "Validation error: Usage ./Harl <level>" << std::endl;
     return false;
   }
 
   const std::string level = argv[1];
-  if (!isLevelValid(level))
-  {
-    std::cout << "Validation error: Choose level `DEBUG`, `INFO`, `WARNING` or `ERROR`" << std::endl;
+  if (!isLevelValid(level)) {
+    std::cerr << "Validation error: Choose level `DEBUG`, `INFO`, `WARNING` or `ERROR`" << std::endl;
     return false;
   }
 
@@ -40,16 +34,13 @@ bool isInputValid(int argc, char **argv)
 }
 
 // ------------------ RUN ------------------ //
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
-  if (!isInputValid(argc, argv))
-  {
+  if (!isInputValid(argc, argv)) {
     return 1;
   }
 
   Harl h = Harl();
-
   const std::string level = argv[1];
   h.complain(level);
 
