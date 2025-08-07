934ac1f032fb182e51e9ce3d5c0374e23edcc309
Ilia Munaev <ilyamunaev@gmail.com>
2025-08-07 09:53:39 +0300

    Add: Form class, exceptions, getters

--- CPP_05/ex01/Form.cpp
+++ CPP_05/ex01/Form.cpp
@@ -1,2 +1,63 @@
 #include "Form.hpp"
 
+// Constructors/Destructors
+Form::Form(const std::string& name, const int gradeToSign, const int gradeToExecute)
+  : m_name(name), m_isSigned(false), m_gradeToSign(gradeToSign), m_gradeToExecute(gradeToExecute) {
+    if (gradeToSign < 1 || gradeToExecute < 1) {
+      throw Form::GradeTooHighException();
+    }
+    if (gradeToSign > 150 || gradeToExecute > 150) {
+      throw Form::GradeTooLowException();
+    }
+}
+
+Form::~Form() {}
+
+// Getters
+std::string Form::getName() const {
+  return m_name ;
+}
+
+int Form::getGradeToSign() const {
+  return m_gradeToSign ;
+}
+
+int Form::getGradeToExecute() const {
+  return m_gradeToExecute ;
+}
+
+bool Form::getIsSigned() const {
+  return m_isSigned ;
+}
+
+// Exceptions
+Form::GradeTooHighException::GradeTooHighException()
+  : m_msg("Error: grade is too high, should be greater or equal 1") {}
+
+  const char* Form::GradeTooHighException::what() const noexcept {
+  return m_msg.c_str();
+}
+
+Form::GradeTooLowException::GradeTooLowException()
+  : m_msg("Error: grade is too low, should be less than or equal 150") {}
+
+  const char* Form::GradeTooLowException::what() const noexcept {
+  return m_msg.c_str();
+}
+
+// Overload
+std::ostream& operator<<(std::ostream& os, const Form& f) {
+  std::string signStatus;
+  if (f.getIsSigned()) {
+    signStatus = "True";
+  } else {
+    signStatus = "False";
+  }
+  os << "Form: `" << f.getName()
+      << "`, Signed: `"<< signStatus
+      << "`, Grade to sign: `" << f.getGradeToSign()
+      << "`, Grade to Execute: `" << f.getGradeToExecute()
+      << "`." << std::endl;
+  return os;
+}
+
--- CPP_05/ex01/Form.hpp
+++ CPP_05/ex01/Form.hpp
@@ -1,2 +1,44 @@
-#pragma onece
+#pragma once
+
+#include <iostream>
+
+class Form {
+  public:
+    Form() = delete;
+    Form(const Form& other) = delete;
+    Form& operator=(const Form& other) = delete;
+    Form(const std::string& name, const int gradeToSign, const int gradeToExecute);
+    ~Form();
+
+    std::string getName() const;
+    int getGradeToSign() const;
+    int getGradeToExecute() const;
+    bool getIsSigned() const;
+
+    class GradeTooHighException : public std::exception {
+      public:
+        GradeTooHighException();
+        const char* what() const noexcept override;
+
+      private:
+        const std::string m_msg;
+    };
+
+    class GradeTooLowException : public std::exception {
+      public:
+        GradeTooLowException();
+        const char* what() const noexcept override;
+
+      private:
+        const std::string m_msg;
+    };
+
+  private:
+    const std::string m_name;
+    bool m_isSigned; // false default
+    int m_gradeToSign;
+    int m_gradeToExecute;
+};
+
+std::ostream& operator<<(std::ostream& os, const Form& f);
 
