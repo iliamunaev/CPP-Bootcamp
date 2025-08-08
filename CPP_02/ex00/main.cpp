799e6ba6e5c937da5a46e65835fee49eddb31632
Ilia Munaev <imunaev-@c1r5p17.hive.fi>
2025-08-08 09:45:07 +0300

    Refactor: remove comments

--- CPP_02/ex00/main.cpp
+++ CPP_02/ex00/main.cpp
@@ -11,17 +11,12 @@
 #include "Fixed.hpp"
 
 int main(void) {
-  std::cout << "\n[TEST] Default constructor:" << std::endl;
   Fixed a;
-
-  std::cout << "\n[TEST] Copy constructor:" << std::endl;
   Fixed b(a);
-
-  std::cout << "\n[TEST] Assignment operator:" << std::endl;
   Fixed c;
+
   c = b;
 
-  std::cout << "\n[RESULTS] Raw bits comparison:" << std::endl;
   std::cout << "a.getRawBits(): " << a.getRawBits() << std::endl;
   std::cout << "b.getRawBits(): " << b.getRawBits() << std::endl;
   std::cout << "c.getRawBits(): " << c.getRawBits() << std::endl;
